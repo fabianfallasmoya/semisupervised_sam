ebfaff05fe5d390fa75e445f61717b150f5d485a
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2023-11-15 21:24:44 -0600

    update engine methods

--- engine/__init__.py
+++ engine/__init__.py
@@ -3,4 +3,8 @@ from .early_stopping import EarlyStopper
 from .easyfsl_utils import *
 from .fewshot_model import *
 from .prototypical_networks import *
+from .matching_networks import *
+from .relational_networks import *
+from .bdcspn import *
 from .sam_model import *
+from .fastsam_model import *
