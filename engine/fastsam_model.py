22ba530c3c2d43d39b5ea158b04d1f9a8cab0a3f
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2023-12-04 21:31:42 -0600

    adding sam embeddings

--- engine/fastsam_model.py
+++ engine/fastsam_model.py
@@ -13,25 +13,18 @@ class FASTSAM:
         self.checkpoint = 'weights/FastSAM.pt'
         self.model = FastSAM(self.checkpoint)
 
+        # ------------------------------------------------
+        # Config for SAM model for the embeddings
         if args.sam_model == 'b':
-            self.checkpoint_sam = "weights/sam_vit_b_01ec64.pth"
-            self.model_type = "vit_b"
+            self.checkpoint_embeddings = "weights/sam_vit_b_01ec64.pth"
+            self.model_type_embeddings = "vit_b"
         elif args.sam_model == 'h':
-            self.checkpoint_sam = "weights/sam_vit_h_4b8939.pth"
-            self.model_type = "vit_h"
+            self.checkpoint_embeddings = "weights/sam_vit_h_4b8939.pth"
+            self.model_type_embeddings = "vit_h"
         else:
             RuntimeError("No sam config found")
-        self.model_sam = sam_model_registry[self.model_type](checkpoint=self.checkpoint_sam)#.to('cuda')
-        self.mask_generator = None
+        self.model_embeddings = sam_model_registry[self.model_type_embeddings](checkpoint=self.checkpoint_embeddings).to(args.device)
 
-        # get the size of the embeddings
-        new_pil = Image.new(mode="RGB", size=(200,200))
-        predictor = SamPredictor(self.model_sam) 
-
-        predictor.set_image(np.array(new_pil))
-        # features shape: ([1, 256, 64, 64]), we keep 256
-        self.features_size = predictor.features.shape[1]
-        predictor.reset_image()
 
     def load_simple_mask(self):
         #There are several tunable parameters in automatic mask generation that control 
@@ -44,9 +37,9 @@ class FASTSAM:
 
         #Rerun the following with a few settings, ex. 0.86 & 0.9 for iou_thresh
         # and 0.92 and 0.96 for score_thresh
-
-        mask_generator_ = SamAutomaticMaskGenerator(
-            model=self.model_sam,
+        # SAM embeddings
+        mask_generator_embeddings = SamAutomaticMaskGenerator(
+            model=self.model,
             points_per_side=32,
             # pred_iou_thresh=0.9,
             # stability_score_thresh=0.96,
@@ -55,8 +48,8 @@ class FASTSAM:
             min_mask_region_area=100,  # Requires open-cv to run post-processing
             output_mode="coco_rle",
         )
-        self.mask_generator = mask_generator_
-
+        self.mask_generator_embeddings = mask_generator_embeddings
+        
     def get_unlabeled_samples(self, 
             batch, idx, transform, use_sam_embeddings
         ):
@@ -70,9 +63,7 @@ class FASTSAM:
 
         # batch[0] has the images    
         img = batch[0][idx].cpu().numpy().transpose(1,2,0)
-        print("Numpy image size: ", img.shape)
         img_pil = Image.fromarray(img)
-        print("Image size: ", img_pil.size)
 
         # run sam to create proposals
         #masks = self.mask_generator.generate(img)
@@ -106,24 +97,24 @@ class FASTSAM:
         Return
         :torch of the embeddings from SAM.
         """
-        self.mask_generator.predictor.set_image(img)
-        embeddings = self.mask_generator.predictor.features
+        self.mask_generator_embeddings.predictor.set_image(img)
+        embeddings = self.mask_generator_embeddings.predictor.features
 
         with torch.no_grad():
             _pool = torch.nn.AdaptiveAvgPool2d((1, 1))
             avg_pooled = _pool(embeddings).view(embeddings.size(0), -1)
-        self.mask_generator.predictor.reset_image()
+        self.mask_generator_embeddings.predictor.reset_image()
         return avg_pooled
 
     def get_features(self, img):
         """
-        Receive an image and return the feature embeddings.
+        Receive an image and return the feature maps.
 
         Params
         :img (numpy.array) -> image.
         Return
         :torch of the embeddings from SAM.
         """
-        self.mask_generator.predictor.set_image(img)
-        embeddings = self.mask_generator.predictor.features
+        self.mask_generator_embeddings.predictor.set_image(img)
+        embeddings = self.mask_generator_embeddings.predictor.features
         return embeddings
