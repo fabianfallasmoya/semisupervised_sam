e5545320f4ebbbf3a4faca5c73ee7e83f1c89de7
Danny Xie Li <51223285+dnnxl@users.noreply.github.com>
2023-12-26 17:19:20 -0600

    fix fastsam bug

--- engine/fastsam_model.py
+++ engine/fastsam_model.py
@@ -12,18 +12,21 @@ class FASTSAM:
         self.device = args.device
         self.checkpoint = 'weights/FastSAM.pt'
         self.model = FastSAM(self.checkpoint)
+        self.use_sam_embeddings = False
 
         # ------------------------------------------------
         # Config for SAM model for the embeddings
-        if args.sam_model == 'b':
-            self.checkpoint_embeddings = "weights/sam_vit_b_01ec64.pth"
-            self.model_type_embeddings = "vit_b"
-        elif args.sam_model == 'h':
-            self.checkpoint_embeddings = "weights/sam_vit_h_4b8939.pth"
-            self.model_type_embeddings = "vit_h"
-        else:
-            RuntimeError("No sam config found")
-        self.model_embeddings = sam_model_registry[self.model_type_embeddings](checkpoint=self.checkpoint_embeddings).to(args.device)
+        if args.use_sam_embeddings == 1:
+            self.use_sam_embeddings = True
+            if args.sam_model == 'b':
+                self.checkpoint_embeddings = "weights/sam_vit_b_01ec64.pth"
+                self.model_type_embeddings = "vit_b"
+            elif args.sam_model == 'h':
+                self.checkpoint_embeddings = "weights/sam_vit_h_4b8939.pth"
+                self.model_type_embeddings = "vit_h"
+            else:
+                RuntimeError("No sam config found")
+            self.model_embeddings = sam_model_registry[self.model_type_embeddings](checkpoint=self.checkpoint_embeddings).to(args.device)
 
 
     def load_simple_mask(self):
@@ -38,18 +41,20 @@ class FASTSAM:
         #Rerun the following with a few settings, ex. 0.86 & 0.9 for iou_thresh
         # and 0.92 and 0.96 for score_thresh
         # SAM embeddings
-        mask_generator_embeddings = SamAutomaticMaskGenerator(
-            model=self.model_embeddings,
-            points_per_side=32,
-            # pred_iou_thresh=0.9,
-            # stability_score_thresh=0.96,
-            # crop_n_layers=1, default:0
-            # crop_n_points_downscale_factor=1,default:1
-            min_mask_region_area=100,  # Requires open-cv to run post-processing
-            output_mode="coco_rle",
-        )
-        self.mask_generator_embeddings = mask_generator_embeddings
-        
+        if self.use_sam_embeddings:
+            # SAM embeddings
+            mask_generator_embeddings = SamAutomaticMaskGenerator(
+                model=self.model,
+                points_per_side=32,
+                # pred_iou_thresh=0.9,
+                # stability_score_thresh=0.96,
+                # crop_n_layers=1, default:0
+                # crop_n_points_downscale_factor=1,default:1
+                min_mask_region_area=100,  # Requires open-cv to run post-processing
+                output_mode="coco_rle",
+            )
+            self.mask_generator_embeddings = mask_generator_embeddings
+
     def get_unlabeled_samples(self, 
             batch, idx, transform, use_sam_embeddings
         ):
@@ -67,16 +72,24 @@ class FASTSAM:
 
         # run sam to create proposals
         #masks = self.mask_generator.generate(img)
-        everything_results = self.model(img_pil)#, retina_masks=True, conf=0.1, iou=0.2, imgsz=896)
+        #everything_results = self.model(img_pil)#, retina_masks=True, conf=0.1, iou=0.2, imgsz=896)
+        everything_results = self.model(img_pil, device=self.device, retina_masks=True, imgsz=1024, conf=0.4, iou=0.9,)
+
         #prompt_process = FastSAMPrompt(img_pil, everything_results,  device=self.device)
         #prompt_process.everything_prompt()
         results = everything_results[0].boxes
 
-        for xyxy, xywh, score in zip(results.xyxy, results.xywh, results.conf):
+        #for _, xywh, score in zip(results.xyxy, results.xywh, results.conf):
+        for _, data, score in zip(results.xyxy, results.data, results.conf):
             #xyxy = torchvision.ops.box_convert(
             #    torch.tensor(xywh), in_fmt='xywh', out_fmt='xyxy'
             #)
-            crop = img_pil.crop(np.array(xyxy.cpu().round().long()))  
+            bbox = data.numpy()[:4]
+            xywh = torchvision.ops.box_convert(
+                    torch.tensor(bbox), in_fmt='xyxy', out_fmt='xywh'
+            )
+            
+            crop = img_pil.crop(bbox)  
             if use_sam_embeddings:
                 sample = transform.preprocess_sam_embed(crop)
             else:
@@ -84,7 +97,7 @@ class FASTSAM:
 
             # accumulate
             imgs.append(sample)
-            box_coords.append(xywh.tolist())
+            box_coords.append([round(v) for v in xywh.tolist()])
             scores.append(float(score.item()))
         return imgs, box_coords, scores
 
